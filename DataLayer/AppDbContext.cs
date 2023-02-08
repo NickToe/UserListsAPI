58bf7c990e09bef3ce68d4bda5b9edd4819e0797
Nikita Novozhilov <nikitkann0861@outlook.com>
2023-02-09 00:30:33 +0300

    Fix DbContext constructor

--- DataLayer/AppDbContext.cs
+++ DataLayer/AppDbContext.cs
@@ -5,18 +5,8 @@ namespace UserListsAPI.DataLayer;
 
 public class AppDbContext : DbContext
 {
-  private readonly IConfiguration _configuration;
   public DbSet<Game> Games { get; set; } = null!;
   public DbSet<Movie> Movies { get; set; } = null!;
 
-  public AppDbContext(IConfiguration configuration)
-  { 
-    _configuration = configuration;
-  }
-
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-  {
-    IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").Build();
-    optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
-  }
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 }
