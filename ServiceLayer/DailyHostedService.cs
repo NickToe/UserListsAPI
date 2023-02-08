a2ba63f13204eade53b7dc5e02222fe95e2fb9b5
Nikita Novozhilov <nikitkann0861@outlook.com>
2023-02-09 00:22:00 +0300

    Add CORs Add background service to fetch updates from steam api Move sensitive data from appsettings.json to UserSecrets

---
+++ ServiceLayer/DailyHostedService.cs
@@ -0,0 +1,36 @@
+﻿using UserListsAPI.DataLayer.Entities;
+
+namespace UserListsAPI.ServiceLayer;
+
+public class DailyHostedService : BackgroundService
+{
+  private readonly ILogger<DailyHostedService> _logger;
+  private readonly IServiceProvider _serviceProvider;
+  public DailyHostedService(ILogger<DailyHostedService> logger, IServiceProvider serviceProvider)
+  {
+    _logger = logger;
+    _serviceProvider = serviceProvider;
+  }
+
+  protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+  {
+    while (!stoppingToken.IsCancellationRequested)
+    {
+      try
+      {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+          // call
+          var gameService = scope.ServiceProvider.GetRequiredService<IItemService<Game>>();
+          await gameService.UpdateAll();
+        }
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError("Exception while handling daily task: {exception}", ex.Message);
+      }
+      TimeSpan timeSpan = new TimeOnly(0, 0, 0) - TimeOnly.FromDateTime(DateTime.Now);
+      await Task.Delay(timeSpan);
+    }
+  }
+}
